fix normalization and training

src/features/layers.py
@@ -15,14 +15,14 @@ class GCN(nn.Module):
 
         self.weight = nn.Parameter(torch.FloatTensor(in_features, out_features))
         self.bias = nn.Parameter(torch.FloatTensor(out_features))
-        self.bn = nn.BatchNorm1d(out_features)
+        self.norm = nn.BatchNorm2d(out_features)
 
         # Store the adjacency matrix as an attribute of the model
 
         self.reset_parameters()
 
     def reset_parameters(self):
-        nn.init.xavier_uniform_(self.weight)
+        nn.init.kaiming_uniform_(self.weight, nonlinearity="relu")
         nn.init.zeros_(self.bias)
 
     def forward(self, input_features, adj):
@@ -32,7 +32,7 @@ class GCN(nn.Module):
         if self.bias is not None:
             output = output + self.bias
 
-        output = self.bn(output.transpose(1, 2)).transpose(1, 2)
+        output = self.norm(output.transpose(1, -1)).transpose(1, -1)
         output = F.relu(output)
         output = F.dropout(output, self.dropout, training=self.training)
 
@@ -40,14 +40,15 @@ class GCN(nn.Module):
 
 
 class TCN(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=(1, 7), stride=(1, 1), use_non_parametric=False, dropout=.0):
+    def __init__(self, in_channels, out_channels, in_frames, kernel_size=(1, 7), stride=(1, 1), use_non_parametric=False, dropout=.0):
         super(TCN, self).__init__()
 
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.kernel_size = kernel_size
         self.dropout = dropout
-        self.bn = nn.BatchNorm2d(out_channels)
+        # self.norm = nn.LayerNorm([out_channels, 17, in_frames // kernel_size[-1]])
+        self.norm = nn.BatchNorm2d(out_channels)
 
         if use_non_parametric:
             self.conv = nn.AvgPool2d(kernel_size=kernel_size, stride=stride)
@@ -59,10 +60,11 @@ class TCN(nn.Module):
             self.reset_parameters()
 
     def reset_parameters(self):
-        self.conv.weight.data.normal_(0, 0.01)
+        nn.init.kaiming_uniform_(self.conv.weight, nonlinearity='relu')
+        nn.init.constant_(self.conv.bias, 0)
 
     def forward(self, x):
-        out = F.dropout(self.sigma(self.bn(self.conv(x))), self.dropout, self.training)
+        out = F.dropout(self.sigma(self.norm(self.conv(x))), self.dropout, self.training)
         return out
 
 
@@ -87,15 +89,15 @@ class NodeResidualBlock(nn.Module):
 
 
 class NodeEdgeTCN(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=(1, 7), stride=(1, 1), use_non_parametric=True, dropout=.0):
+    def __init__(self, in_channels, out_channels, in_frames, kernel_size=(1, 7), stride=(1, 1), use_non_parametric=True, dropout=.0):
         super(NodeEdgeTCN, self).__init__()
 
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.kernel_size = kernel_size
         self.dropout = dropout
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.norm1 = nn.BatchNorm2d(out_channels)
+        self.norm2 = nn.BatchNorm2d(out_channels)
 
         if use_non_parametric:
             self.conv_node = nn.AvgPool2d(kernel_size=kernel_size, stride=stride)
@@ -108,12 +110,16 @@ class NodeEdgeTCN(nn.Module):
             self.reset_parameters()
 
     def reset_parameters(self):
-        self.conv_node.weight.data.normal_(0, 0.01)
-        self.conv_edge.weight.data.normal_(0, 0.01)
+        nn.init.kaiming_uniform_(self.conv_node.weight, nonlinearity='relu')
+        nn.init.kaiming_uniform_(self.conv_edge.weight, nonlinearity='relu')
+
+        if self.conv_node.bias is not None:
+            nn.init.constant_(self.conv_node.bias, 0)
+            nn.init.constant_(self.conv_edge.bias, 0)
 
     def forward(self, x, z):
-        out_node = F.dropout(self.sigma(self.bn1(self.conv_node(x))), self.dropout, self.training)
-        out_edge = F.dropout(self.sigma(self.bn2(self.conv_node(z))), self.dropout, self.training)
+        out_node = F.dropout(self.sigma(self.norm1(self.conv_node(x))), self.dropout, self.training)
+        out_edge = F.dropout(self.sigma(self.norm2(self.conv_edge(z))), self.dropout, self.training)
         return out_node, out_edge
 
 
@@ -137,7 +143,7 @@ class NodeEdgeConv(nn.Module):
 
         if node_layer:
             self.node_layer = True
-            self.weight = nn.Parameter(torch.FloatTensor(in_features_v, self.num_node_groups * out_features_v))
+            self.weight = nn.Parameter(torch.FloatTensor(in_features_v, out_features_v))
             self.weight_sec = nn.Parameter(torch.FloatTensor(in_features_e, in_features_v))
             if bias:
                 self.bias = nn.Parameter(torch.FloatTensor(out_features_v))
@@ -145,7 +151,7 @@ class NodeEdgeConv(nn.Module):
                 self.register_parameter('bias', None)
         else:
             self.node_layer = False
-            self.weight = nn.Parameter(torch.FloatTensor(in_features_e, self.num_edge_groups * out_features_e))
+            self.weight = nn.Parameter(torch.FloatTensor(in_features_e, out_features_e))
             self.weight_sec = nn.Parameter(torch.FloatTensor(in_features_v, in_features_e))
             if bias:
                 self.bias = nn.Parameter(torch.FloatTensor(out_features_e))
@@ -155,33 +161,26 @@ class NodeEdgeConv(nn.Module):
         self.reset_parameters()
 
     def reset_parameters(self):
-        stdv = 1. / math.sqrt(self.weight.size(1))
-        self.weight.data.uniform_(-stdv, stdv)
-
-        stdv = 1. / math.sqrt(self.weight_sec.size(1))
-        self.weight_sec.data.uniform_(-stdv, stdv)
+        nn.init.kaiming_uniform_(self.weight, nonlinearity='relu')
+        nn.init.kaiming_uniform_(self.weight_sec, nonlinearity='relu')
 
         if self.bias is not None:
-            self.bias.data.uniform_(-stdv, stdv)
+            nn.init.zeros_(self.bias)
 
     def forward(self, H_v, H_e, adj_e, adj_v, T):
-        batch_size = H_v.shape[0]
-        num_joints = adj_v.shape[-1]
-        num_edges = adj_e.shape[-1]
-
         if self.node_layer:
             '''
             \sigma( ̃ A_v ( H_v + W_s (T H_e) ) W_v)
             '''
-            msg_to_nodes = T @ H_e # B x V x F
-            msg_to_nodes_t = msg_to_nodes.view(batch_size, -1, msg_to_nodes.shape[-1]) @ self.weight_sec
+            E, V = adj_e.shape[0], adj_v.shape[0]
+            msg_to_nodes = T @ H_e  # B x V x F
+            msg_to_nodes_t = msg_to_nodes.view(-1, H_e.shape[1], V, H_e.shape[-1]) @ self.weight_sec
             node_feat = H_v + msg_to_nodes_t
 
             # node transformation
             temp = (node_feat @ self.weight)
-            temp = temp.reshape(batch_size, num_joints, self.num_node_groups, self.out_features_v)
-            temp = temp.transpose(-3, -2)
-            output = torch.sum(adj_v @ temp, dim=-3)
+
+            output = adj_v @ temp
 
             if self.bias is not None:
                 ret = output + self.bias
@@ -190,15 +189,13 @@ class NodeEdgeConv(nn.Module):
             '''
             \sigma(T.t() Φ(H_v P_v) T * ̃ A_e H_e W_e)
             '''
-
+            E, V = adj_e.shape[0], adj_v.shape[0]
             msg_to_edges = T.t() @ H_v  # B x E x F
-            msg_to_edges_t = msg_to_edges @ self.weight_sec # b x V x F'
+            msg_to_edges_t = msg_to_edges.view(-1, H_e.shape[1], E, H_v.shape[-1]) @ self.weight_sec  # b x V x F'
             edge_feat = H_e + msg_to_edges_t
 
             temp = (edge_feat @ self.weight)
-            temp = temp.reshape(batch_size, num_edges, self.num_edge_groups, self.out_features_e)
-            temp = temp.transpose(-3, -2)
-            output = torch.sum(adj_e @ temp, dim=-3)
+            output = adj_e @ temp
 
             if self.bias is not None:
                 ret = output + self.bias
@@ -206,7 +203,15 @@ class NodeEdgeConv(nn.Module):
 
 
 class GCNodeEdgeModule(nn.Module):
-    def __init__(self, in_frames, nfeat_v, nfeat_e, nhid_v, nhid_e, dropout, use_bn=True, use_activ=True):
+    def __init__(self,
+                 in_frames,
+                 nfeat_v,
+                 nfeat_e,
+                 nhid_v,
+                 nhid_e,
+                 dropout,
+                 use_norm=True,
+                 use_activ=True,):
         super(GCNodeEdgeModule, self).__init__()
 
         # node
@@ -215,23 +220,23 @@ class GCNodeEdgeModule(nn.Module):
         # edge
         self.gc_edge = NodeEdgeConv(nhid_v, nhid_v, nfeat_e, nhid_e, node_layer=False)
 
-        self.use_bn = use_bn
+        self.use_norm = use_norm
         self.use_activ = use_activ
         self.in_frames = in_frames
         self.nhid_v = nhid_v
         self.nhid_e = nhid_e
 
-        if self.use_bn:
-            self.bn1 = nn.BatchNorm1d(nhid_v)
-            self.bn2 = nn.BatchNorm1d(nhid_e)
+        if self.use_norm:
+            self.norm1 = nn.BatchNorm2d(nhid_v)
+            self.norm2 = nn.BatchNorm2d(nhid_e)
 
         self.dropout = dropout
 
     def forward(self, X, Z, adj_e, adj_v, T):
         X, Z = self.gc_node(X, Z, adj_e, adj_v, T)
 
-        if self.use_bn:
-            X = self.bn1(X.transpose(1, 2)).transpose(1, 2)
+        if self.use_norm:
+            X = self.norm1(X.transpose(1, -1)).transpose(1, -1)
         if self.use_activ:
             X, Z = F.relu(X), F.relu(Z)
 
@@ -240,8 +245,8 @@ class GCNodeEdgeModule(nn.Module):
 
         X, Z = self.gc_edge(X, Z, adj_e, adj_v, T)
 
-        if self.use_bn:
-            Z = self.bn2(Z.transpose(1, 2)).transpose(1, 2)
+        if self.use_norm:
+            Z = self.norm2(Z.transpose(1, -1)).transpose(1, -1)
         if self.use_activ:
             X, Z = F.relu(X), F.relu(Z)
 
@@ -305,7 +310,8 @@ class STGConv(nn.Module):
                  num_stages=1,
                  residual=False,
                  use_non_parametric=False,
-                 use_edge_conv=True):
+                 use_edge_conv=True,
+                 aggregate=True,):
         super(STGConv, self).__init__()
 
         self.adj_v = nn.ParameterList([nn.Parameter(adj_v.clone().detach()) for i in range(num_stages)])
@@ -316,6 +322,13 @@ class STGConv(nn.Module):
         self.n_in_frames = n_in_frames
         self.tcn_window = tcn_window
         self.use_edge_conv = use_edge_conv
+        self.aggregate = aggregate
+        self.nhid_v = nhid_v
+        self.nhid_e = nhid_e
+
+        self.seq_len = self.n_in_frames // self.frame_length
+        self.n_nodes_in_seq = self.frame_length * 17
+        self.n_edges_in_seq = self.frame_length * 16
 
         self.graph_stages = nn.ModuleList()
         for s in range(num_stages):
@@ -329,45 +342,69 @@ class STGConv(nn.Module):
                 )
 
         # Add a non parametric option here
-        if use_edge_conv:
+        if use_edge_conv and aggregate:
             self.tcn = NodeEdgeTCN(nhid_v,
                                    nhid_v,
+                                   in_frames=n_in_frames,
                                    kernel_size=(1, self.tcn_window),
                                    stride=(1, self.tcn_window),
                                    use_non_parametric=use_non_parametric,
                                    dropout=dropout)
-        else:
+        elif aggregate:
             self.tcn = TCN(nhid_v,
                            nhid_v,
+                           in_frames=n_in_frames,
                            kernel_size=(1, self.tcn_window),
                            stride=(1, self.tcn_window),
                            use_non_parametric=use_non_parametric,
                            dropout=dropout)
 
+    def norm_g(self, g):
+        degrees = torch.sum(g, 1, keepdim=True)
+        degrees[degrees == 0] = 1
+        g = g / degrees
+        return g
+
+    def normalize_undigraph(self, A_hat):
+        if (len(A_hat.shape) == 3):
+            num_group, num_node = A_hat.shape[:2]
+            normed_adj = torch.zeros(num_group, num_node, num_node).to(A_hat.device)
+
+            for grp_id in range(num_group):
+                grp_adj = A_hat[grp_id]
+                normed_adj[grp_id] = self.norm_g(grp_adj)
+
+        elif (len(A_hat.shape) == 2):
+            A_hat = F.relu(A_hat)
+            normed_adj = self.norm_g(A_hat)
+
+        return normed_adj
+
     def forward(self, X, Z=None):
         if self.use_edge_conv:
             for s in range(len(self.graph_stages)):
-                X, Z = self.graph_stages[s](X, Z, self.adj_e[s], self.adj_v[s], self.T)
+                normed_adj_e = self.normalize_undigraph(self.adj_e[s])
+                normed_adj_v = self.normalize_undigraph(self.adj_v[s])
 
-            hdim_v = X.shape[-1]
-            hdim_e = Z.shape[-1]
+                X, Z = X.reshape(-1, self.seq_len, self.n_nodes_in_seq, self.nhid_v), Z.reshape(-1, self.seq_len, self.n_edges_in_seq, self.nhid_v)
+                X, Z = self.graph_stages[s](X, Z, normed_adj_e, normed_adj_v, self.T)
 
-            X, Z = X.reshape(-1, hdim_v, 17, self.n_in_frames), Z.reshape(-1, hdim_e, 16, self.n_in_frames)
-            X, Z = self.tcn(X, Z)
+            if self.aggregate:
+                X, Z = X.reshape(-1, self.n_in_frames, 17, self.nhid_v).transpose(1, -1), Z.reshape(-1, self.n_in_frames, 16, self.nhid_e).transpose(1, -1)
+                X, Z = self.tcn(X, Z)
 
-            n_frames = X.shape[-1]
-            return X.view(-1, n_frames, 17, hdim_v), Z.view(-1, n_frames, 16, hdim_e)
+                X = X.transpose(1, -1)
+                Z = Z.transpose(1, -1)
+            return X, Z
         else:
             for s in range(len(self.graph_stages)):
-                X = self.graph_stages[s](X, self.adj_v[s],)
+                X = X.reshape(-1, self.seq_len, self.n_nodes_in_seq, self.nhid_v)
+                X = self.graph_stages[s](X, self.adj_v[s])
 
-            hdim_v = X.shape[-1]
-
-            X = X.reshape(-1, hdim_v, 17, self.n_in_frames)
-            X = self.tcn(X)
-
-            n_frames = X.shape[-1]
-            return X.view(-1, n_frames, 17, hdim_v), Z
+            if self.aggregate:
+                X = X.reshape(-1, self.n_in_frames, 17, self.nhid_v).transpose(1, -1)
+                X = self.tcn(X).transpose(1, -1)
+            return X, Z
 
     def string(self):
         return f"In Frames: {self.n_in_frames} -> Out Frames: {self.n_in_frames // self.tcn_window}, Processed Window: {self.tcn_window}"

src/features/networks.py
@@ -36,6 +36,7 @@ class TGraphNet(nn.Module):
                  n_outv,
                  gcn_window,
                  tcn_window,
+                 aggregate,
                  in_frames,
                  gconv_stages,
                  dropout,
@@ -56,12 +57,16 @@ class TGraphNet(nn.Module):
         self.infeat_e = infeat_e
         self.use_edge_conv = use_edge_conv
 
+        self.seq_len = in_frames // gcn_window[0]
+        self.n_nodes = 17 * gcn_window[0]
+        self.n_edges = 16 * gcn_window[0]
+
         self.pre = GCNodeEdgeModule(in_frames, infeat_v, infeat_e, nhid_v[0], nhid_e[0], dropout=0) if use_edge_conv else GCN(in_frames, infeat_v, nhid_v[0], dropout=0)
         self.layers = nn.ModuleList()
 
         n_stages = len(nhid_v)
         for i in range(n_stages):
-            adj_v, adj_e, T = Graph(17, 16, self.gcn_window[i])()
+            adj_v, adj_e, T = Graph(17, 16, self.gcn_window[i], norm=True)()
 
             self.layers.append(
                 STGConv(
@@ -77,7 +82,8 @@ class TGraphNet(nn.Module):
                     num_stages=gconv_stages[i],
                     residual=use_residual_connections,
                     use_non_parametric=use_non_parametric,
-                    use_edge_conv=use_edge_conv),
+                    use_edge_conv=use_edge_conv,
+                    aggregate=aggregate[i]),
                 )
 
         self.post_node = nn. Sequential(
@@ -90,7 +96,6 @@ class TGraphNet(nn.Module):
             self.post_edge = nn.Sequential(
                 nn.Linear(n_outv * 17 + 16 * nhid_e[-1], nhid_e[-1]),
                 nn.ReLU(),
-                nn.Dropout(dropout),
                 nn.Linear(nhid_e[-1],  n_oute * 16)
             )
         else:
@@ -98,7 +103,6 @@ class TGraphNet(nn.Module):
             self.post_edge = nn.Sequential(
                 nn.Linear(n_outv * 17 + 17 * nhid_v[-1], nhid_v[-1]),
                 nn.ReLU(),
-                nn.Dropout(dropout),
                 nn.Linear(nhid_v[-1], n_oute * 16)
             )
 
@@ -106,14 +110,11 @@ class TGraphNet(nn.Module):
         if self.use_edge_conv:
             assert Z is not None, "If edge conv is used then edge features must be passed as input"
 
-            X, Z = X.view(-1, (self.gcn_window[0] * 17), self.infeat_v), Z.view(-1, (self.gcn_window[0] * 16), self.infeat_e)
+            X, Z = X.reshape(-1, self.seq_len, self.n_nodes, self.infeat_v), Z.reshape(-1, self.seq_len, self.n_edges, self.infeat_e)
             X, Z = self.pre(X, Z, self.adj_e, self.adj_v, self.T)
 
             for s in range(len(self.layers) - 1):
                 X, Z = self.layers[s](X, Z)
-
-                # reshape to have B, (T[l] * n_nodes), F for the next layer
-                X, Z = X.view(-1, (self.gcn_window[s+1] * 17), X.shape[-1]), Z.view(-1, (self.gcn_window[s+1] * 16), Z.shape[-1])
 
             X, Z = self.layers[-1](X, Z)
 
@@ -126,12 +127,11 @@ class TGraphNet(nn.Module):
 
             return X.view(batch_size, 17, -1), Z
         else:
-            X = X.view(-1, (self.gcn_window[0] * 17), self.infeat_v)
+            X = X.reshape(-1, self.seq_len, self.n_nodes, self.infeat_v)
             X = self.pre(X, self.adj_v)
 
             for s in range(len(self.layers) - 1):
                 X, _ = self.layers[s](X)
-                X = X.view(-1, (self.gcn_window[s+1] * 17), X.shape[-1])
 
             features, _ = self.layers[-1](X)
             X = self.post_node(features)

src/models/train.py
@@ -21,6 +21,7 @@ from data.h36m_dataset import Human36M
 from common.h36m_skeleton import joint_id_to_names
 from data.generators import ChunkedGenerator_Seq, UnchunkedGenerator_Seq, ChunkedGenerator_Frame, eval_data_prepare
 import loss
+from common.utils import change_momentum
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -63,15 +64,16 @@ def train(model, optimizer, loss_fn, train_gen, metrics, params, epoch, writer, 
     for cameras_train, batch_3d, batch_6d, batch_2d, batch_edge in train_gen.next_epoch():
         input_2d = torch.FloatTensor(batch_2d).to(device)
         input_edge = torch.FloatTensor(batch_edge).to(device)
-        target_pose_3d = torch.FloatTensor(batch_3d).to(device).reshape(-1, 17, 3)
-        target_angle_6d = torch.FloatTensor(batch_6d).to(device).reshape(-1, 16, 6)
+        target_pose_3d = torch.FloatTensor(batch_3d).to(device)
+        target_angle_6d = torch.FloatTensor(batch_6d).to(device)
 
         predicted_pos3d, predicted_angle_6d = model(input_2d, input_edge)
+        predicted_pos3d[:, 0] = 0 # 0 out hip pos
 
-        # middle_index = int((params.in_frames - 1) / 2)
+        middle_index = int((target_pose_3d.shape[1] - 1) / 2)
 
-        # target_angle_6d = out_6d[:, middle_index].view_as(predicted_angle_6d)
-        # target_pose_3d = out_3d[:, middle_index].view_as(predicted_pos3d)
+        target_angle_6d = target_angle_6d[:, middle_index].view_as(predicted_angle_6d)
+        target_pose_3d = target_pose_3d[:, middle_index].view_as(predicted_pos3d)
 
         batch_size = input_2d.shape[0]
         hip_ori = torch.tensor([[[1., 0., 0., 1., 0., 0.]]]).repeat(batch_size, 1, 1).to(device)
@@ -128,6 +130,11 @@ def train(model, optimizer, loss_fn, train_gen, metrics, params, epoch, writer, 
                   'train_geodesic_err: {:.4f}'.format(mean_geodesic_distance.item()),
                   'time: {:.4f}s'.format(time.time() - t)
                   )
+
+    # Decay BatchNorm momentum
+    # momentum = 0.1 * np.exp(-(epoch + 1) / params.num_epochs * np.log(0.1 / 0.001))
+    # changer = change_momentum(momentum)
+    # model.apply(changer)
 
     # mean metrics
     metrics_loss_mean = np.mean([x['train_loss'] for x in summary])
@@ -188,21 +195,22 @@ def evaluate(model, loss_fn, val_gen, metrics, params, epoch, writer, log_dict, 
         for cameras_train, batch_3d, batch_6d, batch_2d, batch_edge in val_gen.next_epoch():
             input_2d = torch.FloatTensor(batch_2d).to(device)
             input_edge = torch.FloatTensor(batch_edge).to(device)
-            target_pose_3d = torch.FloatTensor(batch_3d).to(device).reshape(-1, 17, 3)
-            target_angle_6d = torch.FloatTensor(batch_6d).to(device).reshape(-1, 16, 6)
+            target_pose_3d = torch.FloatTensor(batch_3d).to(device)
+            target_angle_6d = torch.FloatTensor(batch_6d).to(device)
 
-            # out_3d, out_6d, input_edge, input_2d = eval_data_prepare(params.in_frames, input_2d, input_edge, out_3d, out_6d)
-            # out_3d = out_3d.to(device)
-            # out_6d = out_6d.to(device)
-            # input_edge = input_edge.to(device)
-            # input_2d = input_2d.to(device)
+            out_3d, out_6d, input_edge, input_2d = eval_data_prepare(params.in_frames, input_2d, input_edge, target_pose_3d, target_angle_6d)
+            target_pose_3d = out_3d.to(device)
+            target_angle_6d = out_6d.to(device)
+            input_edge = input_edge.to(device)
+            input_2d = input_2d.to(device)
 
             predicted_pos3d, predicted_angle_6d = model(input_2d, input_edge)
+            predicted_pos3d[:, 0] = 0 # 0 out hip pos
 
-            # middle_index = int((params.in_frames - 1) / 2)
+            middle_index = int((target_pose_3d.shape[1] - 1) / 2)
 
-            # target_angle_6d = out_6d[:, middle_index].view_as(predicted_angle_6d)
-            # target_pose_3d = out_3d[:, middle_index].view_as(predicted_pos3d)
+            target_angle_6d = target_angle_6d[:, middle_index].view_as(predicted_angle_6d)
+            target_pose_3d = target_pose_3d[:, middle_index].view_as(predicted_pos3d)
 
             batch_size = input_2d.shape[0]
             hip_ori = torch.tensor([[[1., 0., 0., 1., 0., 0.]]]).repeat(batch_size, 1, 1).to(device)
@@ -518,7 +526,7 @@ def main():
         # train_generator = ChunkedGenerator_Seq(params.batch_size//params.stride, cameras=None, poses_2d=pos2d, poses_3d=pos3d, rot_6d=angles_6d,
         #                                        edge_feat=edge_features, chunk_length=params.in_frames, pad=0, shuffle=True,)
         train_generator = ChunkedGenerator_Frame(params.batch_size // params.stride, cameras=None, poses_2d=pos2d, poses_3d=pos3d, rot_6d=angles_6d,
-                                                 edge_feat=edge_features, chunk_length=1, pad=((params.in_frames - 1) // 2), shuffle=True,)
+                                                 edge_feat=edge_features, chunk_length=11, pad=35, shuffle=True,)
 
         logging.info(f"N Frames: {train_generator.num_frames()}, N Batches {train_generator.num_batches}")
         logging.info("- done.")
@@ -526,9 +534,9 @@ def main():
     logging.info("Loading test dataset....")
     test_dataset = Human36M(data_dir=args.data_dir, train=False, ds_category=params.ds_category)
     pos2d, pos3d, angles_6d, edge_features = test_dataset.pos2d, test_dataset.pos3d_centered, test_dataset.gt_angles_6d, test_dataset.edge_features
-    # val_generator = UnchunkedGenerator_Seq(cameras=None, poses_2d=pos2d, poses_3d=pos3d, rot_6d=angles_6d, edge_feat=edge_features)
-    val_generator = ChunkedGenerator_Frame(params.batch_size // params.stride, cameras=None, poses_2d=pos2d, poses_3d=pos3d, rot_6d=angles_6d,
-                                           edge_feat=edge_features, chunk_length=1, pad=((params.in_frames - 1) // 2), shuffle=False,)
+    val_generator = UnchunkedGenerator_Seq(cameras=None, poses_2d=pos2d, poses_3d=pos3d, rot_6d=angles_6d, edge_feat=edge_features)
+    # val_generator = ChunkedGenerator_Frame(params.batch_size // params.stride, cameras=None, poses_2d=pos2d, poses_3d=pos3d, rot_6d=angles_6d,
+    #                                        edge_feat=edge_features, chunk_length=5, pad=38, shuffle=False,)
 
     logging.info("Number of validation frames: {}".format(val_generator.num_frames()))
     logging.info("- done.")
@@ -544,6 +552,7 @@ def main():
                       tcn_window=params.tcn_window,
                       in_frames=params.in_frames,
                       gconv_stages=params.gconv_stages,
+                      aggregate=params.aggregate,
                       dropout=params.dropout,
                       use_residual_connections=params.use_residual_connections,
                       use_non_parametric=params.use_non_parametric,